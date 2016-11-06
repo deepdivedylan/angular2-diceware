ff41f6dd6b61ada63e39051de458bbc4ac9ab2b3
Dylan McDonald <dmcdonald21@cnm.edu>
2016-11-05 18:41:13 -0600

    added simple table view

--- public_html/templates/splash.php
+++ public_html/templates/splash.php
@@ -1,1 +1,8 @@
 <h1>Splash Component</h1>
+<table class="table table-bordered table-responsive table-striped table-word-wrap">
+	<tr><th>Roll</th><th>Word</th></tr>
+	<tr *ngFor="let dicewareRoll of dicewareRolls">
+		<td>{{ dicewareRoll.roll }}</td>
+		<td>{{ dicewareRoll.word }}</td>
+	</tr>
+</table>
