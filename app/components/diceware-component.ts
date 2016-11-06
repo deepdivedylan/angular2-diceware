440e8ee0122dfb7b0315b3dd468afe3751bbbc92
Dylan McDonald <dmcdonald21@cnm.edu>
2016-11-05 21:20:48 -0600

    indent :heart_eyes_cat:

--- app/components/diceware-component.ts
+++ app/components/diceware-component.ts
@@ -1,7 +1,22 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
+import {DicewareService} from "../services/diceware-service";
+import {Diceware} from "../classes/diceware";
 
 @Component({
 	templateUrl: "./templates/diceware.php"
 })
 
-export class DicewareComponent {}
+export class DicewareComponent implements OnInit {
+	diceware: Diceware = new Diceware("", "");
+
+	constructor(private dicewareService: DicewareService, private route: ActivatedRoute) {}
+
+	ngOnInit() : void {
+		this.route.params.forEach((params : Params) => {
+			let roll = +params["roll"];
+			this.dicewareService.getDiceware(roll)
+				.subscribe(diceware => this.diceware = diceware);
+		});
+	}
+}
