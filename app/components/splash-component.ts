3a354597c302dd5ba61532460eab90282fb8c229
Dylan McDonald <dmcdonald21@cnm.edu>
2016-11-05 18:25:28 -0600

    autoloaded the diceware data on init

--- app/components/splash-component.ts
+++ app/components/splash-component.ts
@@ -1,7 +1,18 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Diceware} from "../classes/diceware";
+import {DicewareService} from "../services/diceware-service";
 
 @Component({
 	templateUrl: "./templates/splash.php"
 })
 
-export class SplashComponent {}
+export class SplashComponent implements OnInit {
+	dicewareRolls: Diceware[] = [];
+
+	constructor(private dicewareService: DicewareService) {}
+
+	ngOnInit() : void {
+		this.dicewareService.getAllDiceware()
+			.subscribe(dicewareRolls => this.dicewareRolls = dicewareRolls);
+	}
+}
