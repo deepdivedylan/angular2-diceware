a28282557192fe3f2266c28bcce225702890b8f2
Dylan McDonald <dmcdonald21@cnm.edu>
2016-11-05 21:34:32 -0600

    added usable diceware tempate

--- public_html/templates/diceware.php
+++ public_html/templates/diceware.php
@@ -1,1 +1,8 @@
-<h1>Diceware Component</h1>
+<h1>Diceware Roll</h1>
+<p>
+	<em>Roll:</em> {{ diceware.roll }}<br />
+	<em>Word:</em> {{ diceware.word }}
+</p>
+<p>
+	<a routerLink=""><i class="fa fa-home" aria-hidden="true"></i> Return Home</a>
+</p>
