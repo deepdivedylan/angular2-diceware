543c5319c858324ff17b75d278a89abbae9b3417
Dylan McDonald <dmcdonald21@cnm.edu>
2016-11-05 21:48:10 -0600

    added a blurb

--- public_html/templates/splash.php
+++ public_html/templates/splash.php
@@ -1,4 +1,9 @@
-<h1>Splash Component</h1>
+<h1>Diceware Search</h1>
+<p>
+	<a href="http://world.std.com/~reinhold/diceware.html">Diceware</a> is a site that allows users to use five dice to create a password. The word list at the original site is buried deep within several pages of text. We have made the word list easily viewable and searchable.
+</p>
+<p>Enjoy!</p>
+<hr />
 <div class="row">
 	<div class="col-md-6">
 		<div class="input-group">
