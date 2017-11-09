c7e164638d4b89ee92ccd21642ef919e61d1acfd
Dylan McDonald <dmcdonald21@cnm.edu>
2017-11-08 18:48:11 -0700

    updated to load Angular on the ready event

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 import { enableProdMode } from '@angular/core';
 
-enableProdMode();
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
+window.addEventListener("DOMContentLoaded", event => {
+	// enableProdMode();
+	const platform = platformBrowserDynamic();
+	platform.bootstrapModule(AppModule);
+});
