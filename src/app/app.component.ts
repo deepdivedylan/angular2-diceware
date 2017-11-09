efa41bdb9cde0e14939be25eb54b5d375d1243e9
Dylan McDonald <dmcdonald21@cnm.edu>
2017-11-08 19:02:02 -0700

    got rid of dead code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import {Component} from "@angular/core";
 	templateUrl: './templates/diceware-app.html'
 })
 
-export class AppComponent {
-	navCollapse = true;
-
-	toggleCollapse() {
-		this.navCollapse = !this.navCollapse;
-	}
-}
+export class AppComponent {}
